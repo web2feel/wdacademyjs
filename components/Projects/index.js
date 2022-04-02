3a84f335609f93176a12af92cfd5265242bc9f2e
Jinson <web2feel@gmail.com>
2022-04-02 22:31:33 +0530

    linting bug fix

--- components/Projects/index.js
+++ components/Projects/index.js
@@ -14,7 +14,7 @@ function Projects() {
             </SectionTitle>
             <p className="w-4/5 mb-4">
               The most obvious reason for learning JavaScript is if you have
-              hopes of becoming a web developer. Even if you haven't got your
+              hopes of becoming a web developer. Even if you havent got your
               heart set on a tech career, being proficient in this language will
               enable you to build websites from scratch—a pretty useful skill to
               have in todays job market!
@@ -22,7 +22,7 @@ function Projects() {
             <p className="w-4/5 mb-4">
               If you want to become a Full Stack developer, you must have a
               decent knowledge about JS. Once you know the JS, you can explore
-              more on Vue js , React JS, Node JS, Mongo DB etc
+              more on Vue js , React JS, Node JS, MongoDB etc
             </p>
           </div>
           <div className='w-full sm:w-2/3 '>
